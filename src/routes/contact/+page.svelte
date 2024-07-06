7f1454c25977833d9f69fbbcb1a0fa787449f4f9
Glenn Sweeney <glenn@glennsweeney.com>
2024-07-05 19:27:42 -0700

    Visual tweaks (#21)

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,16 @@
 <main>glenn@glennsweeney.com</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        font-variation-settings:
+            "wght" 300,
+            "wdth" 90;
+        font-size: 5vw;
+        color: rgb(150, 150, 150);
+        margin-top: 6rem;
+    }
+</style>
