f8590cb66d7c12ea24ad157897040813ec666ce1
Glenn Sweeney <glenn@glennsweeney.com>
2024-07-10 19:02:16 -0700

    Add dbg page

---
+++ src/routes/dbg/+page.svelte
@@ -0,0 +1,18 @@
+<main>
+    <!-- Simple front-on camera -->
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 76 54"
+        ><path
+            fill="none"
+            stroke="#000000"
+            stroke-linejoin="round"
+            d="M 4 18 L 11 18 L 11 14 L 20 14 L 20 18 L 31 18 L 31 13 L 37 4 L 49 4 L 55 13 L 55 18 L 59 18 L 59 16 L 69 16 L 69 18 L 72 18 L 72 50 L 4 50 L 4 18"
+        />
+        <circle cx="43" cy="34" r="13" fill="none" stroke="#000000" /></svg
+    >
+</main>
+
+<style>
+    svg {
+        width: 20rem;
+    }
+</style>
