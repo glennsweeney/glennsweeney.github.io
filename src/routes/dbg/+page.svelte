004eee7fad20be5e177ea2815cd42d2e7b6da32e
Glenn Sweeney <glenn@glennsweeney.com>
2024-07-10 19:37:29 -0700

    Add another svg

--- src/routes/dbg/+page.svelte
+++ src/routes/dbg/+page.svelte
@@ -9,6 +9,15 @@
         />
         <circle cx="43" cy="34" r="13" fill="none" stroke="#000000" /></svg
     >
+    <!-- Side profile DSLR -->
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 86 64"
+        ><path
+            fill="none"
+            stroke="#000000"
+            stroke-linejoin="round"
+            d="M 4 6 L 25 8 L 26 10 L 28 12 L 31 12 L 32 13 L 39 13 L 40 14 L 46 14 L 47 15 L 53 16 L 53 13 L 49 12 L 49 6 L 55 4 L 73 4 L 75 9 L 79 9 L 80 8 L 82 8 L 82 17 L 78 18 L 80 20 L 80 44 L 77 52 L 59 52 L 57 45 L 53 44 L 53 41 L 47 42 L 46 43 L 40 43 L 39 44 L 32 44 L 31 45 L 28 45 L 26 47 L 25 49 L 4 51 L 4 49 L 16 41 A 16 16 0 0 1 16 16 L 4 8 L 4 6"
+        /></svg
+    >
 </main>
 
 <style>
