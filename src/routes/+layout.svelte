d87ed22faf234e4767c2eef3b6f4de06a28aa6f5
Glenn Sweeney <glenn@glennsweeney.com>
2025-08-11 20:35:14 -0400

    Add non-default font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,14 @@
 
 <svelte:head>
     <link rel="icon" href={favicon} />
+    <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet"
+    />
+    <link rel="stylesheet" href="/global.css" />
 </svelte:head>
 
 {@render children?.()}
