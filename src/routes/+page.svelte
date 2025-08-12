debe7c51f4185272d1e8b0f15323c38851a46bb1
Glenn Sweeney <glenn@glennsweeney.com>
2025-08-12 13:13:08 -0400

    Add color palette examples

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Palette from "$lib/Palette.svelte";
+</script>
+
+<Palette colors={["#86AB90", "#BA6645", "#2E3331", "#6F8DA0", "#CFA43C", "#2E3331"]} />
+
+<div style="height: 64px;"></div>
+
+<Palette colors={["#7fa48e", "#c06c4a", "#2E3331", "#5e7480", "#b8923a", "#2E3331"]} />
+
+<div style="height: 64px;"></div>
+
+<Palette colors={["#8b915e", "#9e4f3b", "#2E3331", "#607784", "#b06f45", "#2E3331"]} />
+
+<div style="height: 64px;"></div>
+
+<Palette colors={["#6f8a5f", "#7d543a", "#2E3331", "#5f7482", "#c49a3a", "#2E3331"]} />
+
+<div style="height: 64px;"></div>
+
+<Palette colors={["#9fb089", "#b87a61", "#2E3331", "#6d8792", "#c79e3b", "#2E3331"]} />
